8e2d912be36209575941eeefc5a05470d3e3a6f3
marleyholt <leaog.8@gmail.com>
2026-09-09 13:19:18 -0300

    Enviando meu client personalizado para gerar APK

---
+++ init.lua
@@ -0,0 +1,83 @@
+-- CONFIG
+APP_NAME = "MarleyOT" -- cria a pasta %appdata%/MARLEYOT sem conflitar com outros Tibias
+APP_VERSION = 772     -- versão 8.00
+DEFAULT_LAYOUT = "retro" -- layout nostálgico clássico
+
+-- Serviços web desativados para não dar delay de conexão
+Services = {
+  website = "",
+  updater = "",
+  news = "",
+  stats = "",
+  crash = "",
+  feedback = ""
+}
+
+-- Servidores pré-configurados: O seu IP da Oracle Cloud 137.131.196.66 na porta 7171 versão 800!
+Servers = {
+  MARLEYOT = "137.131.196.66:7171:772"
+}
+ALLOW_CUSTOM_SERVERS = false -- Trava para os jogadores não precisarem digitar nada!
+-- CONFIG END
+
+g_app.setName("Marley OT")
+-- CONFIG END
+
+-- print first terminal message
+g_logger.info(os.date("== application started at %b %d %Y %X"))
+g_logger.info(g_app.getName() .. ' ' .. g_app.getVersion() .. ' rev ' .. g_app.getBuildRevision() .. ' (' .. g_app.getBuildCommit() .. ') made by ' .. g_app.getAuthor() .. ' built on ' .. g_app.getBuildDate() .. ' for arch ' .. g_app.getBuildArch())
+
+if not g_resources.directoryExists("/data") then
+  g_logger.fatal("Data dir doesn't exist.")
+end
+
+if not g_resources.directoryExists("/modules") then
+  g_logger.fatal("Modules dir doesn't exist.")
+end
+
+-- settings
+g_configs.loadSettings("/config.otml")
+
+-- set layout
+local settings = g_configs.getSettings()
+local layout = DEFAULT_LAYOUT
+if g_app.isMobile() then
+  layout = "mobile"
+elseif settings:exists('layout') then
+  layout = settings:getValue('layout')
+end
+g_resources.setLayout(layout)
+
+-- load mods
+g_modules.discoverModules()
+g_modules.ensureModuleLoaded("corelib")
+  
+local function loadModules()
+  -- libraries modules 0-99
+  g_modules.autoLoadModules(99)
+  g_modules.ensureModuleLoaded("gamelib")
+
+  -- client modules 100-499
+  g_modules.autoLoadModules(499)
+  g_modules.ensureModuleLoaded("client")
+
+  -- game modules 500-999
+  g_modules.autoLoadModules(999)
+  g_modules.ensureModuleLoaded("game_interface")
+
+  -- mods 1000-9999
+  g_modules.autoLoadModules(9999)
+end
+
+-- report crash
+if type(Services.crash) == 'string' and Services.crash:len() > 4 and g_modules.getModule("crash_reporter") then
+  g_modules.ensureModuleLoaded("crash_reporter")
+end
+
+-- run updater, must use data.zip
+if type(Services.updater) == 'string' and Services.updater:len() > 4 
+  and g_resources.isLoadedFromArchive() and g_modules.getModule("updater") then
+  g_modules.ensureModuleLoaded("updater")
+  return Updater.init(loadModules)
+end
+loadModules()
